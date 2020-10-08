784b6334be5a2b92215c1ca24156ca56778800d0
bbbbbr <bbbbbr@users.noreply.github.com>
2020-10-07 20:10:56 -0700

    Switch to using records, sort output

--- romusage.c
+++ romusage.c
@@ -20,15 +20,16 @@
 
 
 int handle_args(int, char * []);
-void add_rec(char * str_area, long value);
+void add_rec(char * str_area, long addr, long size);
 static int area_rec_compare(const void* a, const void* b);
 void print_all_recs(void);
 void parse_symfile_line(char * str_in);
 
 
 #define MAX_STR_LEN     4096
+#define MAX_STR_AREALEN 100
 #define MAX_SPLIT_WORDS 6
-#define MAX_AREAS       20
+#define MAX_AREAS       30
 
 enum {
     USAGE_NONE,
@@ -37,8 +38,9 @@ enum {
 };
 
 typedef struct area_rec {
-    char str_area[MAX_STR_LEN];
-    long value;
+    char str_area[MAX_STR_AREALEN];
+    long addr;
+    long size;
 } area_rec;
 
 area_rec area_list[MAX_AREAS];
@@ -47,17 +49,16 @@ int area_count = 0;
 int usage_mode = USAGE_NONE; // default to usage turned off
 
 
-void add_rec(char * str_area, long value) {
+void add_rec(char * str_area, long addr, long size) {
     int c;
 
     // check to see if key matches any entries
     for (c = 0; c < area_count; c++) {
 
-        // if a match is found, add it
+        // prevent duplicates, reject if entry already exists
         if (0 == strncmp(area_list[c].str_area,
                          str_area,
                          sizeof(area_list[c].str_area))) {
-            area_list[c].value += value;
             return;
         }
     }
@@ -68,7 +69,8 @@ void add_rec(char * str_area, long value) {
         strncpy(area_list[c].str_area,
                 str_area,
                 sizeof(area_list[c].str_area));
-        area_list[c].value = value;
+        area_list[c].addr = addr;
+        area_list[c].size = size;
     }
 }
 
@@ -93,27 +95,28 @@ void print_all_recs(void) {
     qsort (area_list, area_count, sizeof(area_rec), area_rec_compare);
 
     if (usage_mode == USAGE_NONE) {
-        fprintf(stdout,"Area         Size\n"
-                       "-----        ----\n");
+        fprintf(stdout,"Area        Addr                Size \n"
+                       "-----       -----------------   -----\n");
     } else {
-        fprintf(stdout,"Area         Size    Used  Remains\n"
-                       "-----        ----    ----  -------\n");
+        fprintf(stdout,"Area        Addr                Size         Used  Remains\n"
+                       "-----       -----------------   -----        ----  -------\n");
     }
 
 
     // Print all records
     for (c = 0; c < area_count; c++) {
-        fprintf(stdout,"%-12s %-7ld ",area_list[c].str_area, area_list[c].value);   // Area, Size
+            fprintf(stdout,"%-13s",area_list[c].str_area); // Area name
+            fprintf(stdout,"%6lx -> %6lx",area_list[c].addr,
+                                         area_list[c].addr + area_list[c].size -1 ); // Address Start -> End
+            fprintf(stdout,"%8ld bytes",area_list[c].size); // Area size
+
 
         // Only print utilization for CODE areas
         if ((usage_mode != USAGE_NONE)  &&
             strstr(area_list[c].str_area, "CODE")) {
 
-            // Percentage
-            fprintf(stdout,"%%%-4ld ", (area_list[c].value * 100) / area_size);
-
-            // Free
-            fprintf(stdout,"%-5ld ", area_size - area_list[c].value);
+            fprintf(stdout,"  %%%-5ld", (area_list[c].size * 100) / area_size); // Percentage
+            fprintf(stdout,"%5ld ", area_size - area_list[c].size); // Remaining
         }
 
         fprintf(stdout,"\n");
@@ -142,25 +145,7 @@ void parse_symfile_line(char * str_in) {
         !(strstr(str_in, "HEADER")) && // Exclude HEADER areas
         !(strtol(p_words[3], NULL, 10) == 0))  // Exclude empty areas
      {
-        // Don't repeat areas (can be duplicated across multiple output "pages"
-        if (0 != strncmp(str_area_last,p_words[0], sizeof(str_area_last))) {
-            // [0] = Area Name
-            // [1] = Hex Address Start
-            // [2] = Hex Size
-            // [3] = Decimal Size
-            // [4] = 'bytes'
-            // [5] = '(ABS/REL,CON)'
-            fprintf(stdout,"%-12s:",p_words[0]);   // [0] = Area
-            fprintf(stdout,"%8lx - %-8lx",strtol(p_words[1], NULL, 16),
-                                          strtol(p_words[1], NULL, 16) + strtol(p_words[2], NULL, 16) -1 ); // Address Start -> End
-            fprintf(stdout,"%8ld %s",strtol(p_words[3], NULL, 10), p_words[4]);
-
-            fprintf(stdout, "\n");
-        }
-
-        // Copy area name to check against next pass
-        strncpy(str_area_last, p_words[0], sizeof(str_area_last));
-//        add_rec(p_words[0], strtol(p_words[3], NULL, 16)); // [1] = Area, [3] = Hex Size string converted to decimal
+        add_rec(p_words[0], strtol(p_words[1], NULL, 16), strtol(p_words[3], NULL, 10)); // [0] Area, [1] Hex Address, [3] Decimal Size
     }
 
 
@@ -208,7 +193,9 @@ int handle_args( int argc, char * argv[] ) {
                    "Use: Pipe symbol files in to parse them.\n"
                    "Example: \"more build/*.sym build/res/*.sym | romusage\"\n"
                    "\n"
-                   "Note: Usage estimates are for Area only, and do not factor in whether multiple areas share the same bank (such as HOME, CODE, GS_INIT, etc).\n"
+                   "Note: Usage estimates are for a given Area only.\n"
+                   "They **do not** factor in whether multiple areas share\n"
+                   "the same bank (such as HOME, CODE, GS_INIT, etc).\n"
                    );
             return false;  // Don't parse input when -h is used
         }
